e3dbc00e88b4acf7d90891ce7f3e6e23ec37734d
AnujN <natraj.anuj@gmail.com>
2024-03-15 21:11:38 -0500

    Modifications for video

--- RL/throw_test.py
+++ RL/throw_test.py
@@ -5,52 +5,53 @@ import numpy as np
 from stable_baselines3 import SAC
 import csv
 
-# env = gym.make("PandaThrow-v3", render_mode="human", control_type="joints")
-env = gym.make("PandaThrow-v3", render_mode="human")
+env = gym.make("PandaThrow-v3", render_mode="human", control_type="joints")
+# env = gym.make("PandaThrow-v3", render_mode="human")
 env.reset()
 
-# models_dir = "models/SAC_1710493867"
+models_dir = "models/SAC_1710493867"
 # models_dir = "models/SAC_latest"
-models_dir = "models/SAC_1710487905"
+# models_dir = "models/SAC_1710487905"
 logdir = "logs"
 
 # 590000 was working well for 905
-model_path = f"{models_dir}/590000.zip"
+model_path = f"{models_dir}/160000.zip"
 model = SAC.load(model_path, env=env)
 
 episodes = 10
 
 vec_env = model.get_env()
 obs = vec_env.reset()
-count = 0
-first = 0
-step = 0
+# count = 0
+# first = 0
+# step = 0
+time.sleep(4)
 
 with open('ee_pose.csv', 'w') as file:
-    writer = csv.writer(file)
+    # writer = csv.writer(file)
     for ep in range(episodes):
-        if first < 1:
-            writer.writerow([f"{obs['observation'][0,0]},{obs['observation'][0,1]},"
-                            +f"{obs['observation'][0,2]},{obs['observation'][0,3]},"
-                            +f"{obs['observation'][0,4]},{obs['observation'][0,5]},"
-                            +f"{obs['observation'][0,6]}"])
-            first += 1
+        # if first < 1:
+        #     writer.writerow([f"{obs['observation'][0,0]},{obs['observation'][0,1]},"
+        #                     +f"{obs['observation'][0,2]},{obs['observation'][0,3]},"
+        #                     +f"{obs['observation'][0,4]},{obs['observation'][0,5]},"
+        #                     +f"{obs['observation'][0,6]}"])
+        #     first += 1
         obs = vec_env.reset()
         done = False
         print("Iterate")
         print(f"{obs['observation'][0]}")
         while not done:
             vec_env.render()
-            time.sleep(0.8)
-            print(step)
+            time.sleep(0.7)
+            # print(step)
             action, _states = model.predict(obs, deterministic = True)
             obs, reward, done, info = vec_env.step(action)
-            step += 1
-            if count < 1:
-                print(f"{obs['observation'][0]}")   
-                writer.writerow([f"{obs['observation'][0,0]},{obs['observation'][0,1]},"
-                            +f"{obs['observation'][0,2]},{obs['observation'][0,3]},"
-                            +f"{obs['observation'][0,4]},{obs['observation'][0,5]},"
-                            +f"{obs['observation'][0,6]}"])
-        count += 1
+            # step += 1
+        #     if count < 1:
+        #         print(f"{obs['observation'][0]}")   
+        #         writer.writerow([f"{obs['observation'][0,0]},{obs['observation'][0,1]},"
+        #                     +f"{obs['observation'][0,2]},{obs['observation'][0,3]},"
+        #                     +f"{obs['observation'][0,4]},{obs['observation'][0,5]},"
+        #                     +f"{obs['observation'][0,6]}"])
+        # count += 1
     vec_env.close()
